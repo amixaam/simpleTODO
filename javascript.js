c87494d1e17945c29cd8dfefcdd9065c0e5a65a5
amixam <60570885+amixaam@users.noreply.github.com>
2023-04-18 16:34:58 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
 function remove() {
-
+    console.log("remove");
 }
 
 function add() {
-
+    let value = document.getElementById("todo-input").value;
+    if (value === "") { return; }
+    
+    document.getElementById("todo-input").value = '';
+    let listItem = document.createElement("li");
+    listItem.textContent = value;
+    let list = document.getElementById("list").appendChild(listItem)
 }
